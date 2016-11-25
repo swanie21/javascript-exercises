ddbf0dc43af4af6d4d9373825af405c7e007eed9
swanie21 <kswanie21@gmail.com>
2016-11-25 12:10:02 -0700

    add two exercises

---
+++ capitalize-first-letter.js
@@ -0,0 +1,13 @@
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// Write a JavaScript function to capitalize the first letter of a string
---
+++ split-string-into-array.js
@@ -0,0 +1,9 @@
+function splitString(string) {
+  return string.trim().split('');
+}
+
+const splitString = (string) => {
+  return string.trim().split('');
+};
+
+// Write a JavaScript function to split a string and convert it into an array of letters
